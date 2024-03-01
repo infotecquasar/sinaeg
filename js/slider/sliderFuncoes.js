93d3f5c0d074e3d7f1f44b29e0fdaede9037762f
C1260311 Leonardo Augusto da Silva <c1260311@interno.bb.com.br>
2024-03-01 11:30:18 -0300

    class='hero-slider' apenas sem o estilo

--- js/slider/sliderFuncoes.js
+++ js/slider/sliderFuncoes.js
@@ -2,7 +2,7 @@ function carregarSliderPorDiv(idDiv,nomeFotoComCaminho,totalFotos) {
     var outputDiv = document.getElementById(idDiv);
     if(outputDiv){ 
         var result = "<section class='slider'>";
-                result += "<div class='hero-slider'>";
+                result += "<div class='hero-slider' style='-webkit-box-shadow: 0px 10px 13px -7px #000000, 10px 17px 20px -9px rgba(59,83,255,1.0);box-shadow: 0px 10px 13px -7px #000000, 10px 17px 20px -9px rgba(59,83,255,1.0);border-radius: 9px;'>";
         
         // total de arquivos que existir
         for (var i = 1; i <= totalFotos; i++) {
