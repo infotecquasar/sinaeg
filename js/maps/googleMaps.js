0f24d1306bc0e479c915fcf5704671d8e9207a92
C1260311 Leonardo Augusto da Silva <c1260311@interno.bb.com.br>
2024-05-22 12:09:15 -0300

    ajustes

--- js/maps/googleMaps.js
+++ js/maps/googleMaps.js
@@ -4,7 +4,7 @@ function initMap() {
             // Create a map object and specify the DOM element for display.
             var map = new google.maps.Map(document.getElementById('map'), {
                 center: myLatLng,
-                zoom: 16 // Adjust the zoom level as needed
+                zoom: 18 // Adjust the zoom level as needed
             });
 
             // Create a marker and set its position
