dcf1b188705252503aa452caa59a7ce9d799da9a
C1260311 Leonardo Augusto da Silva <c1260311@interno.bb.com.br>
2024-06-10 23:43:34 -0300

    ajustes

--- js/slider/sliderFuncoes.js
+++ js/slider/sliderFuncoes.js
@@ -1,18 +1,51 @@
-function carregarSliderPorDiv(idDiv,nomeFotoComCaminho,totalFotos) {
-    var outputDiv = document.getElementById(idDiv);
-    if(outputDiv){ 
-        var result = "<section class='slider'>";
+function carregarSliderPorDiv(idDivImagens,idDivImagensTexto,nomeFotoComCaminho,totalFotos) {
+    var outputDivImagens = document.getElementById(idDivImagens);
+    var outputDivImagensTexto = document.getElementById(idDivImagensTexto);
+    if(outputDivImagens){ 
+        var result = "<section class='slider' ;>";
                 result += "<div class='hero-slider' style='-webkit-box-shadow: 0px 10px 13px -7px #000000, 10px 17px 20px -9px rgba(59,83,255,1.0);box-shadow: 0px 10px 13px -7px #000000, 10px 17px 20px -9px rgba(59,83,255,1.0);border-radius: 9px;'>";
         
         // total de arquivos que existir
-        for (var i = 2; i <= totalFotos; i++) {
+        for (var i = 1; i <= totalFotos; i++) {
             var nomeFoto = nomeFotoComCaminho + i + ".jpg";
-            result += "<div class='single-slider' style='background-image:url("+nomeFoto+");'></div>";							
+            result += "<div class='single-slider'>"; 
+            result += "<img src='"+nomeFoto+"' style='height: 120% !important;width: 95% !important' alt='Image 2'></img> ";          						
+            if(i === 1){
+                result += "<div class='caption'>O SINAEG convida todos os seus filiados para Assembleia Extraordinária a realizar-se no dia 20.05.2024, segunda-feira, às 19:00 de forma online. O link será enviado previamente no grupo de mensagens dos filiados do SINAEG. Pauta: 1. Apresentar e votar a contraproposta do SINAEG ao MGI sobre os cargos da ERCE (Lei n⁰ 12.277/2010).</div>";
+            
+            } else {
+                result += "<div class='caption'>SINAEG representando os servidores do ERCE no ano de 2014 com o então Secretário de Relações Institucionais da Presidência da República, o Senhor Ricardo Berzoini.</div>";
+            
+            }
+            
+            result += "</div>";
         }
                 
             result += "</div>";
         result += "</section>";
+
         // Set the inner HTML of the output div to the result string
-        outputDiv.innerHTML = result;
+        outputDivImagens.innerHTML = result;
+
     }
+}
+
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function changeSlide(n) {
+    showSlide(currentSlide + n);
 }
